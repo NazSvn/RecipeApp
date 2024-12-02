dc29cafb5e29164628fbbcd35847fd11cd636b7d
NazSvn <idler90@gmail.com>
2024-12-02 23:30:19 +0800

    feat(navbar): enhance NavBar with styling and search functionality
    - Added `SearchEngine` component for improved search functionality.
    - Updated NavBar layout with responsive styling using Tailwind CSS.
    - Made the NavBar sticky and styled it with a modern appearance.
    - Ensured proper alignment and responsiveness for desktop and mobile views.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import { NavLink } from 'react-router-dom';
+import SearchEngine from './SearchEngine';
 
 const NavBar = () => {
   return (
-    <nav>
-      <h1>Recipes</h1>
+    <nav className='fixed left-0 right-0 bg-slate-700 z-50'>
+      <div className='flex flex-col sm:flex-row p-2 gap-3 mx-auto items-center sm:max-w-4xl justify-between sm:h-20'>
+        <NavLink to={'/'}>
+          <h1 className='text-2xl font-medium '>Recipes</h1>
+        </NavLink>
 
-      <input
-        type='text'
-        name='search'
-        placeholder='Search recipe'
-      />
-      <ul>
-        <li>
-          <NavLink to={'/'}>Recipes</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/favorites'}>Favorites</NavLink>
-        </li>
-      </ul>
+        <SearchEngine />
+        <ul className='flex gap-3'>
+          <li>
+            <NavLink to={'/'}>Recipes</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/favorites'}>Favorites</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
