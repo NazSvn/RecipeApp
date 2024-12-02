9b63e1b25f1e59cccac758b91e0e13a338facccd
NazSvn <idler90@gmail.com>
2024-12-02 23:15:08 +0800

    refactor: Rename RecipeContextProvider file
    - Update import statement for RecipeContextProvider
    - Change import path from 'GlobalContext.jsx' to 'RecipeContextProvider.jsx'

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import RecipeContextProvider from './context/GlobalContext.jsx';
+import { BrowserRouter } from 'react-router-dom'; 
+import RecipeContextProvider from './context/RecipeContextProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
