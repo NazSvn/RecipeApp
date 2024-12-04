377e809d34de9c494a001f934e615a9cd2179832
NazSvn <idler90@gmail.com>
2024-12-04 23:40:32 +0800

    feat(favorites): Add favorite toggle functionality to RecipeItem
    - Introduced favorite toggle button with heart icon
    - Implemented tooltip for favorite action
    - Added prop types for isFavorite and toggleFav

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { IoHeart, IoHeartOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
-const RecipeItem = ({ recipe }) => {
+const RecipeItem = ({ recipe, isFavorite, toggleFav }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
   return (
     <>
-      <div className='rounded border relative '>
+      <div className='rounded border relative'>
         <Link to={`/details/${recipe.id}`}>
           <div>
             <img
@@ -16,7 +19,23 @@ const RecipeItem = ({ recipe }) => {
             <div className='text-base '>{recipe.title}</div>
             <div className=' font-bold text-lg'>{recipe.title}</div>
           </div>
-        </Link> 
+        </Link>
+
+        <button
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => setShowTooltip(false)}
+          className='absolute top-2 right-2 items-center justify-center border-slate-900'
+          onClick={() => toggleFav(recipe)}
+        >
+          {isFavorite ? <IoHeart size={28} /> : <IoHeartOutline size={28} />}{' '}
+        </button>
+        <span
+          className={`absolute top-9 font-medium -right-6 w-24 z-10 bg-slate-700 hover:bg-red-400 text-[0.5rem] text-center content-center py-1 border rounded-md ${
+            showTooltip ? 'visible delay-1000 transition-all' : 'hidden'
+          }`}
+        >
+          {isFavorite ? 'Remove from favorites' : 'Add to favorites'}
+        </span>
       </div>
     </>
   );
@@ -25,4 +44,6 @@ export default RecipeItem;
 
 RecipeItem.propTypes = {
   recipe: PropTypes.object.isRequired,
+  isFavorite: PropTypes.bool.isRequired,
+  toggleFav: PropTypes.func.isRequired,
 };
