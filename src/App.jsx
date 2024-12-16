db82b33882a66bac9ad52197eed153b437a05268
NazSvn <idler90@gmail.com>
2024-12-16 17:33:41 +0800

    feat: Add SearchResults route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { Route, Routes } from 'react-router-dom'; 
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Home from './pages/home';
-import Favorites from './pages/Favorites'; 
+import Favorites from './pages/Favorites';
 import Details from './pages/Details';
+import SearchResults from './pages/SearchResults';
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='/favorites'
-          element={<Favorites />}
-        />
-        <Route
-          path='/details/:id'
-          element={<Details />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/searchResults" element={<SearchResults />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/details/:id" element={<Details />} />
       </Routes>
     </>
   );
