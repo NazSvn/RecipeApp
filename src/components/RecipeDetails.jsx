4c50c46bc5b1c98569e9849af9c2a44b734efc21
NazSvn <idler90@gmail.com>
2024-12-23 13:26:51 +0800

    refactor: Update RecipeDetails component with reusable utilities and improved styles
    - Replaced static image rendering with GetRecipeImage utility for dynamic image handling.
    - Introduced DietTypeList component for streamlined rendering of diet tags.
    - Updated layout styles for better responsiveness and alignment across devices.
    - Enhanced button styling and icon colors for improved visual hierarchy.
    - Maintained PropTypes validation for type-checking 'detailsData' prop.

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -7,6 +7,8 @@ import {
 import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { GlobalContext } from '../context/GlobalContext';
+import GetRecipeImage from '../utility/GetRecipeImages';
+import DietTypeList from './DietTypeList';
 
 const RecipeDetails = ({ detailsData }) => {
   const { favorites, toggleFav } = useContext(GlobalContext);
@@ -19,30 +21,39 @@ const RecipeDetails = ({ detailsData }) => {
 
   return (
     <>
-      <section className="bg-slate-400 p-10 sm:h-[500px]">
+      <section className="block content-center p-4 md:h-[500px] md:bg-[#6633993b] md:p-6">
         {RecipeDetails && (
-          <div className="mx-auto grid grid-cols-1 gap-5 sm:max-w-4xl sm:[grid-template-columns:1fr_2fr]">
-            <div className="mx-auto h-60 w-60 md:h-80 md:w-80">
-              <img
-                className="h-60 rounded-md object-cover md:h-80"
-                src={detailsData?.image}
-                alt={`${detailsData?.title}'s image`}
-              />
+          <div className="mx-auto grid grid-cols-1 gap-5 sm:max-w-[85%] md:max-w-4xl md:[grid-template-columns:1fr_2fr]">
+            <div className="mx-auto w-full md:h-80 md:w-80">
+              {detailsData && (
+                <GetRecipeImage
+                  recipe={detailsData}
+                  classes="h-[355px] w-[390px] min-[500px]:w-full rounded-sm sm:rounded-md object-cover md:h-80"
+                />
+              )}
+              <div className="mt-2 text-sm md:text-base">
+                {detailsData?.creditsText}
+              </div>
             </div>
             <div>
-              <div>
-                <h1 className="mb-5 text-3xl">{detailsData?.title}</h1>
-              </div>
+              <h1 className="mb-5 text-3xl">{detailsData?.title}</h1>
+
               <div className="mb-4 flex content-center gap-4">
                 <div>
                   <span>
-                    <IoPieChart size={28} className="mr inline" />
+                    <IoPieChart
+                      size={28}
+                      className="mr inline text-accent-lime"
+                    />
                   </span>{' '}
                   servings: {detailsData?.servings}
                 </div>
                 <div>
                   <span>
-                    <IoTimeOutline size={28} className="mr inline" />
+                    <IoTimeOutline
+                      size={28}
+                      className="mr inline text-accent-lime"
+                    />
                   </span>{' '}
                   Ready in: {detailsData?.readyInMinutes} min
                 </div>
@@ -56,79 +67,23 @@ const RecipeDetails = ({ detailsData }) => {
               />
 
               <div>
-                {detailsData?.diets?.map((tag, i) => (
-                  <div className="mr-4 inline text-sm font-bold" key={i}>
-                    {tag}
-                  </div>
-                ))}
+                {detailsData?.diets &&
+                  detailsData.diets.length > 0 &&
+                  DietTypeList(detailsData?.diets)}
               </div>
               <button
-                className="mt-16 flex h-10 w-52 items-center justify-center gap-2 rounded-md border border-slate-900"
+                className="mt-6 flex h-10 w-52 items-center justify-center gap-2 rounded-md border border-slate-900 bg-[#663399] sm:mt-5"
                 onClick={() => toggleFav(detailsData)}
               >
                 {isFavorite ? (
-                  <IoHeartDislike size={28} />
+                  <IoHeartDislike size={28} className="text-accent-orange" />
                 ) : (
-                  <IoHeartOutline size={28} />
+                  <IoHeartOutline size={28} className="text-accent-orange" />
                 )}{' '}
                 <span>
                   {isFavorite ? 'Remove from favorites' : 'Add to favorites'}
                 </span>
               </button>
-            </div>
-            <div>{detailsData?.creditsText}</div>
-          </div>
-        )}
-      </section>
-
-      <section>
-        {detailsData && (
-          <div className="container mx-auto max-w-4xl px-4 py-8">
-            <div className="overflow-hidden rounded-lg shadow-lg">
-              <div className="">
-                <h2 className="mb-4 border-b pb-2 text-2xl font-semibold">
-                  Ingredients
-                </h2>
-                <ul className="space-y-2 text-sm sm:text-base">
-                  {detailsData?.extendedIngredients?.map((item, index) => (
-                    <li
-                      key={index}
-                      className="ml-4 flex flex-wrap items-center border-b pb-3"
-                    >
-                      <span className="mr-2 font-medium">
-                        <span>{item.amount}</span> <span>{item.unit}</span>
-                      </span>
-                      <span className="">{item.originalName}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-
-              {/* Instructions Section */}
-              <div className="pt-6">
-                <h2 className="mb-4 border-b pb-2 text-2xl font-semibold">
-                  Directions
-                </h2>
-                {detailsData?.analyzedInstructions?.map((instruction, i) => (
-                  <div key={i} className="mb-4">
-                    {instruction.name && (
-                      <h3 className="mb-2 text-xl font-medium">
-                        {instruction.name}
-                      </h3>
-                    )}
-                    <ul className="space-y-2">
-                      {instruction.steps.map((step) => (
-                        <li key={step.number} className="ml-4">
-                          <span className="mr-2 font-semibold">
-                            Step {step.number}
-                          </span>
-                          {step.step}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
-              </div>
             </div>
           </div>
         )}
